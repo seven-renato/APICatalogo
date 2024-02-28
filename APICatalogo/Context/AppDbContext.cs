a0ba0396e48ba622ea04d8ef2b5c990dc00fec2e
Paulo Renato <seven.renato@hotmail.com>
2024-02-28 20:50:47 -0300

    Add project files.

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class AppDbContext : DbContext // Mapeamento do domínio às tabelas do banco de dados, consultas alterações, inclusão, etc
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options ) : base( options )
+    {        
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; } // ? para ser opcionial
+    public DbSet<Produto>? Produtos { get; set; }
+}
---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Models;
+// Classes anemicas
+public class Categoria
+{
+    public int CategoriaId { get; set; } // Ou Id, o entity framework core entende como chave primária
+    public string? Nome { get; set; }
+    public string ImagemUrl { get; set; } = string.Empty;
+}
